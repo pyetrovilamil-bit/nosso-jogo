773ae6e884b82c068d74baabd8ea6657fc64bd4c
pyetrovilamil-bit <pyetro_vilamil@estudante.sesisenai.org.br>
2026-05-26 10:53:08 -0300

    cartas.js

---
+++ cartas.js
@@ -0,0 +1,47 @@
+const cartas = [
+
+{
+  nome:"Oxigênio",
+  simbolo:"O",
+  numero:8,
+  familia:"Calcogênios",
+  ataque:70,
+  defesa:50,
+  raridade:"COMUM",
+  curiosidade:"21% da atmosfera terrestre"
+},
+
+{
+  nome:"Urânio",
+  simbolo:"U",
+  numero:92,
+  familia:"Actinídeos",
+  ataque:98,
+  defesa:90,
+  raridade:"LENDÁRIO",
+  curiosidade:"Usado em energia nuclear"
+},
+
+{
+  nome:"Carbono",
+  simbolo:"C",
+  numero:6,
+  familia:"Não-metal",
+  ataque:65,
+  defesa:60,
+  raridade:"RARO",
+  curiosidade:"Base da vida orgânica"
+},
+
+{
+  nome:"Ferro",
+  simbolo:"Fe",
+  numero:26,
+  familia:"Metal de Transição",
+  ataque:85,
+  defesa:95,
+  raridade:"ÉPICO",
+  curiosidade:"Muito usado na construção"
+}
+
+];
